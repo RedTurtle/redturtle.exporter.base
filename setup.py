ae525a035bfbae73353c5c6bf87b6faec8226ff9
Filippo Campi <filippo.campi@redturtle.it>
2018-06-04 18:39:48 +0200

    [fix] fixed version of genericSetup

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
         # -*- Extra requirements: -*-
         'plone.api',
-        'Products.GenericSetup>=1.8.2',
+        'Products.GenericSetup',
         'setuptools',
         'ploneorg.jsonify',
         'redturtle.portlets.inspector',
