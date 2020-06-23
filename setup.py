3befb5f971e6781cbf57a1c916a6b3c3f23f3502
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-06-23 17:12:09 +0200

    Back to development: 1.1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='redturtle.exporter.base',
-    version='1.1.2',
+    version='1.1.3.dev0',
     description="Json exporter base for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
